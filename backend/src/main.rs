493b5b902ea0c9a2fb580051e41dfe30e40d4abf
elliexcoding <ladieexstepho@gmail.com>
2023-04-01 18:00:22 +0900

    toy query param handler

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,18 +1,29 @@
 #![allow(unused)]
 
-use std::net::SocketAddr;
-use axum::response::Html;
-use axum::Router;
+use axum::extract::Query;
+use axum::response::{Html, IntoResponse};
 use axum::routing::get;
+use axum::Router;
+use serde::Deserialize;
+use std::net::SocketAddr;
 
 #[tokio::main]
 async fn main() {
-    let routes_hello = Router::new().route(
-        "/hello",
-        get(|| async { Html("<h1>Hello, World!</h1>") }),
-    );
+    let routes_hello = Router::new().route("/hello", get(handler_hello));
 
-    let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
+    #[derive(Debug, Deserialize)]
+    struct HelloParams {
+        name: Option<String>,
+    }
+
+    async fn handler_hello(Query(params): Query<HelloParams>) -> impl IntoResponse {
+        println!("->> {:<12} - handle_hello = {params:?}", "HANDLER");
+
+        let name = params.name.unwrap_or_else(|| "World".to_string());
+        Html(format!("<h1>Hello, {name}!</h1>"))
+    }
+
+    let addr = SocketAddr::from(([0, 0, 0, 0], 8080));
     println!("->> LISTENING on {}", addr);
     axum::Server::bind(&addr)
         .serve(routes_hello.into_make_service())
