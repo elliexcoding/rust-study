46ddaacbb56073461e577d311575e3ffd5219e5b
elliexcoding <ladieexstepho@gmail.com>
2023-04-08 10:40:27 +0900

    add in cookies middleware

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -2,12 +2,14 @@
 
 pub use self::error::{Error, Result};
 use axum::extract::{Path, Query};
-use axum::response::{Html, IntoResponse};
+use axum::response::{Html, IntoResponse, Response};
 use axum::routing::{get, get_service};
-use axum::Router;
+use axum::{middleware, Router};
 use serde::Deserialize;
 use std::net::SocketAddr;
+use tower_cookies::CookieManagerLayer;
 use tower_http::services::ServeDir;
+
 
 mod web;
 
@@ -18,8 +20,16 @@ mod error;
 async fn main() {
     let routes_all = Router::new()
         .merge(routes_hello())
+        .merge(web::routes_login::routes())
+        .layer(middleware::map_response(main_response_mapper))
+        .layer(CookieManagerLayer::new())
         .fallback_service(routes_static());
 
+    async fn main_response_mapper(res:Response) -> Response {
+        println!("->> {:<12} - main_response_mapper", "HANDLER");
+        println!();
+        res
+    }
 
     fn routes_static() -> Router {
         Router::new().nest_service("/", get_service(ServeDir::new("./")))
