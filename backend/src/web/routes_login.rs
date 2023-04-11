afa59ac17fbd2ca94a066c9bea28f4a08427c954
elliexcoding <ladieexstepho@gmail.com>
2023-04-11 10:41:26 +0900

    basic auth layer with cookies

---
+++ backend/src/web/routes_login.rs
@@ -0,0 +1,35 @@
+use crate::{Error, Result, web};
+use serde::Deserialize;
+use axum::{Json, Router};
+use serde_json::{json, Value};
+use axum::routing::post;
+use tower_cookies::{Cookie, Cookies};
+
+
+pub fn routes() -> Router {
+    Router::new().route("/api/login", post(api_login))
+}
+
+async fn api_login(cookies: Cookies, payload:Json<LoginPayload>) -> Result<Json<Value>> {
+
+    println!("->> {:<12} - api_login", "HANDLER");
+    if payload.username != "demo1" || payload.pwd != "welcome" {
+        return Err(Error::LoginFail);
+    }
+
+    cookies.add(Cookie::new(web::AUTH_TOKEN, "user-1.exp.sign"));
+
+    let body = Json(json!({
+        "result": {
+            "success": true
+        }
+    }));
+
+    Ok(body)
+}
+
+#[derive(Debug, Deserialize)]
+struct LoginPayload {
+    username: String,
+    pwd: String,
+}
