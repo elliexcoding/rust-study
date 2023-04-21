d8d7c18f6327464d6159cf8667c8424af99cc3b8
elliexcoding <ladieexstepho@gmail.com>
2023-04-21 22:25:44 +0900

    json deserializer added in

---
+++ backend/src/routes/custom_json_extractor.rs
@@ -0,0 +1,34 @@
+use axum::{async_trait, BoxError, http, Json};
+use axum::body::HttpBody;
+use axum::extract::FromRequest;
+use axum::http::Request;
+use serde::Deserialize;
+
+#[derive(Deserialize, Debug)]
+pub struct RequestUser {
+    username: String,
+    password: String,
+}
+
+#[async_trait]
+impl<S, B> FromRequest<S, B> for RequestUser
+    where
+    // these bounds are required by `async_trait`
+        B: HttpBody + Send + 'static,
+        B::Data: Send,
+        B::Error: Into<BoxError>,
+        S: Send + Sync,
+{
+    type Rejection = (http::StatusCode, String);
+
+    async fn from_request(req: Request<B>, state: &S) -> Result<Self, Self::Rejection> {
+        let Json(user) = Json::<RequestUser>::from_request(req, state).await
+            .map_err(|_| (http::StatusCode::BAD_REQUEST, "Missing or invalid body".to_string()))?;
+        Ok(user)
+    }
+}
+
+
+pub async fn custom_json_extractor(user: RequestUser) {
+    dbg!(user);
+}
