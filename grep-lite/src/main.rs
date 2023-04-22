897dbd37ccf2c44900c5660fe2a431711fa05177
elliexcoding <ladieexstepho@gmail.com>
2023-04-22 11:49:29 +0900

    use clap and regex pattern matching to find query

--- grep-lite/src/main.rs
+++ grep-lite/src/main.rs
@@ -1,56 +1,31 @@
 use regex::Regex;
+use clap::{Arg, App};
 
 fn main() {
-  let re = Regex::new("picture").unwrap();
+    let args = App::new("grep-lite")
+        .version("1.0")
+        .about("Searches for patterns in files")
+        .arg(Arg::with_name("pattern")
+            .help("The pattern to search for")
+            .takes_value(true)
+            .required(true))
+        // .arg(Arg::with_name("input")
+        //     .help("File to search")
+        //     .takes_value(true)
+        //     .required(true))
+        .get_matches();
 
+    let pattern = args.value_of("pattern").unwrap();
+    let re = Regex::new(pattern).unwrap();
 
-  let ctx_lines = 2;
-  let needle = "oo";
-  let haystack = "\
-Every face, every shop,
-bedroom window, public-house, and
-dark square is a picture
-feverishly turned--in search of what?
-It is the same with books.
-What do we seek
-through millions of pages?";
+    let quote = "Every face, every shop, bedroom window, public-house, and
+dark square is a picture feverishly turned--in search of what?
+It is the same with books. What do we seek through millions of pages?";
 
-  let mut tags: Vec<usize> = vec![];               // <1>
-  let mut ctx: Vec<Vec<(
-               usize, String)>> = vec![];          // <2>
-
-  for (i, line) in haystack.lines().enumerate() {  // <3>
-    if line.contains(needle) {
-      tags.push(i);
-
-      let v = Vec::with_capacity(2*ctx_lines + 1); // <4>
-      ctx.push(v);
-    }
-  }
-
-  if tags.is_empty() {                             // <5>
-    return;
-  }
-
-  for (i, line) in haystack.lines().enumerate() {  // <6>
-    for (j, tag) in tags.iter().enumerate() {
-      let lower_bound =
-	      tag.saturating_sub(ctx_lines);           // <7>
-      let upper_bound =
-	      tag + ctx_lines;
-
-      if (i >= lower_bound) && (i <= upper_bound) {
-          let line_as_string = String::from(line); // <8>
-          let local_ctx = (i, line_as_string);
-          ctx[j].push(local_ctx);
-      }
-    }
-  }
-
-  for local_ctx in ctx.iter() {
-    for &(i, ref line) in local_ctx.iter() {       // <9>
-      let line_num = i + 1;
-      println!("{}: {}", line_num, line);
+  for line in quote.lines() {
+    match re.find(line) {
+      Some(_) => println!("{}", line),
+      None => (),
     }
   }
 }
