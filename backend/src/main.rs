652ad8ea87441428eff5b241bc04ad258c876501
elliexcoding <ladieexstepho@gmail.com>
2023-04-30 10:02:19 +0900

    factor our routes to merge and import error

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,15 +1,36 @@
 #![allow(unused)]
 
-use axum::extract::Query;
+pub use self::error::{Error, Result};
+use axum::extract::{Path, Query};
 use axum::response::{Html, IntoResponse};
-use axum::routing::get;
+use axum::routing::{get, get_service};
 use axum::Router;
 use serde::Deserialize;
 use std::net::SocketAddr;
+use tower_http::services::ServeDir;
+
+
+// Error module
+mod error;
+mod web;
 
 #[tokio::main]
 async fn main() {
-    let routes_hello = Router::new().route("/hello", get(handler_hello));
+    let routes_all = Router::new()
+        .merge(routes_hello())
+        .fallback_service(routes_static());
+
+
+    fn routes_static() -> Router {
+        Router::new().nest_service("/", get_service(ServeDir::new("./")))
+    }
+
+    // Routes hello
+    fn routes_hello() -> Router {
+        Router::new()
+        .route("/hello", get(handler_hello))
+        .route("/hello2/:name", get(handler_hello2))
+    }
 
     #[derive(Debug, Deserialize)]
     struct HelloParams {
@@ -23,10 +44,14 @@ async fn main() {
         Html(format!("<h1>Hello, {name}!</h1>"))
     }
 
+    async fn handler_hello2(name: Path<String>) -> impl IntoResponse {
+
+    }
+
     let addr = SocketAddr::from(([0, 0, 0, 0], 8080));
     println!("->> LISTENING on {}", addr);
     axum::Server::bind(&addr)
-        .serve(routes_hello.into_make_service())
+        .serve(routes_all.into_make_service())
         .await
         .unwrap();
 }
