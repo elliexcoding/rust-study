22b31c4deb23ab7941a2910f45ab6ea4853989b0
elliexcoding <ladieexstepho@gmail.com>
2023-04-30 16:45:14 +0900

    output confirmation when server starts

--- backend/src/lib.rs
+++ backend/src/lib.rs
@@ -1,11 +1,15 @@
 use crate::routes::create_routes;
+use std::net::SocketAddr;
 
 mod routes;
 
 pub async fn run() {
     let app = create_routes();
 
-    axum::Server::bind(&"0.0.0.0:8080".parse().unwrap())
+    let addr = SocketAddr::from(([0, 0, 0, 0], 8080));
+    println!("->> LISTENING on {addr}\n");
+
+    axum::Server::bind(&addr)
         .serve(app.into_make_service())
         .await
         .unwrap();
