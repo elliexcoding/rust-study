91c6461c83e5eebee4e9d40e8518b237310cb6ab
elliexcoding <ladieexstepho@gmail.com>
2023-05-10 22:26:08 +0900

    json serializer added in

---
+++ backend/src/routes/get_json.rs
@@ -0,0 +1,19 @@
+use axum::Json;
+use serde::Serialize;
+
+#[derive(Clone, Serialize)]
+pub struct Data {
+    message: String,
+    count: i32,
+    username: String,
+}
+
+pub async fn get_json() -> Json<Data>{
+    let data = Data {
+        message: "Hello, world!".to_string(),
+        count: 42,
+        username: "John Doe".to_string(),
+    };
+
+    Json(data)
+}
