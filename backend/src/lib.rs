0391a2a3a58879cc66ae0744ea1992efb445ad83
elliexcoding <ladieexstepho@gmail.com>
2023-05-10 22:24:12 +0900

    hook in db connection to routes

--- backend/src/lib.rs
+++ backend/src/lib.rs
@@ -1,10 +1,16 @@
 use crate::routes::create_routes;
 use std::net::SocketAddr;
+use dotenvy::dotenv;
+use sea_orm::Database;
 
 mod routes;
+mod database;
 
 pub async fn run() {
-    let app = create_routes();
+    dotenv().ok();
+    let database_uri = dotenvy::var("DATABASE_URL").unwrap();
+    let database = Database::connect(database_uri).await.unwrap();
+    let app = create_routes(database).await;
 
     let addr = SocketAddr::from(([0, 0, 0, 0], 8080));
     println!("->> LISTENING on {addr}\n");
