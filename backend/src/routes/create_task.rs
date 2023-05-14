f04bfcf6ddf0efd8f0da25444a5e770359ea4446
elliexcoding <ladieexstepho@gmail.com>
2023-05-14 10:28:16 +0900

    direct dummy db create method to db connection pool

--- backend/src/routes/create_task.rs
+++ backend/src/routes/create_task.rs
@@ -1,5 +1,22 @@
-use axum::Extension;
-use sea_orm::{Database, DatabaseConnection, EntityTrait, FromQueryResult};
+use axum::extract::Extension;
+use axum::extract::State;
+use sea_orm::{ActiveModelTrait, ColumnTrait, DatabaseConnection, EntityTrait, QueryFilter, Set};
+use crate::database::tasks;
 
 
-pub async fn create_task(Extension(database): Extension<DatabaseConnection>) {}
+pub async fn create_task(State(database): State<DatabaseConnection>) {
+    println!("Starting create task!");
+    let new_task = tasks::ActiveModel {
+        priority: Set(Some("high".to_string())),
+        title: Set("Hello, world!".to_string()),
+        description: Set(Some("This is a description".to_string())),
+        ..Default::default()
+    };
+
+    println!("New task was created!");
+
+    dbg!(&database);
+    let result = new_task.save(&database).await.unwrap();
+
+    dbg!(result);
+}
