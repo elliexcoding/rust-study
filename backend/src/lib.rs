57295e2f998b8b3e25e28daef1390204bc3612a6
elliexcoding <ladieexstepho@gmail.com>
2023-05-14 10:27:59 +0900

    revised db connection handler

--- backend/src/lib.rs
+++ backend/src/lib.rs
@@ -1,4 +1,4 @@
-use crate::routes::create_routes;
+// use crate::routes::create_routes;
 use std::net::SocketAddr;
 use dotenvy::dotenv;
 use sea_orm::Database;
@@ -8,9 +8,19 @@ mod database;
 
 pub async fn run() {
     dotenv().ok();
+    println!("Booting up server");
     let database_uri = dotenvy::var("DATABASE_URL").unwrap();
-    let database = Database::connect(database_uri).await.unwrap();
-    let app = create_routes(database).await;
+    println!("{}", &database_uri);
+    println!("Database URI is set");
+    let database = Database::connect(database_uri).await.expect("Failed to initiate the db.");
+    // let database = match database_check {
+    //     Ok(database) => database,
+    //     Err(error) => {
+    //         println!("Error connecting to database: {}", error);
+    //         return;
+    //     }
+    // };
+    let app = routes::create_routes(database).await;
 
     let addr = SocketAddr::from(([0, 0, 0, 0], 8080));
     println!("->> LISTENING on {addr}\n");
