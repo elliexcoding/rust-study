05571281c7248742319b3365f0448960fe8b4599
elliexcoding <ladieexstepho@gmail.com>
2023-07-19 19:55:26 +0900

    feat: IMplement UTC time conversion

--- timekeeper/src/main.rs
+++ timekeeper/src/main.rs
@@ -12,6 +12,27 @@ use std::mem::zeroed;
 
 struct Clock;
 
+
+const NTP_TO_UNIX_SECONDS: u64 = 2_208_988_800;
+#[derive(Default, Debug, Copy, Clone)]
+struct NTPTimestamp {
+  seconds: u32,
+  fraction: u32,
+}
+
+impl From<NTPTimestamp> for DateTime<Utc> {
+  fn from(ntp: NTPTimestamp) -> Self {
+    let secs = ntp.seconds as i64 - NTP_TO_UNIX_SECONDS as i64;
+    let mut nanos = ntp.fraction as f64;
+    nanos *= 1e9;
+    nanos /= 2_f64.powi(32);
+
+    Utc.timestamp(secs, nanos as u32)
+  }
+}
+
+
+
 impl Clock {
     fn get() -> DateTime<Local> {
         Local::now()
@@ -128,7 +149,20 @@ fn main() {
       );
       let t = parser(t_).expect(&err_msg);
   
-      Clock::set(t)
+      Clock::set(t);
+
+      let maybe_error = 
+      std::io::Error::last_os_error();
+      let os_error_code = 
+      &maybe_error.raw_os_error();
+
+      match os_error_code {
+        Some(0) => (),
+        Some(_) => {
+          eprintln!("Warning: {}", maybe_error);
+        },
+        None => (),
+      }
     }
   
     let now = Clock::get();
@@ -138,5 +172,54 @@ fn main() {
       "rfc2822" => println!("{}", now.to_rfc2822()),
       "rfc3339" => println!("{}", now.to_rfc3339()),
       _ => unreachable!(),
+    }
+
+
+    fn check_time() -> Result<f64, std::io::Error> {
+      const NTP_PORT: u16 = 123;
+
+      let servers = [
+        "time.nist.gov",
+        "time.apple.com",
+        "time.euro.apple.com",
+        "time.google.com",
+        "time2.google.com",
+      ];
+
+      let mut times = Vec::with_capacity(servers.len);
+
+      for &server in servers.iter() {
+        print!("{}: ", server);
+      }
+
+      let calc = ntp_roundtirp(&server, NTP_PORT);
+
+      match calc {
+        Ok(time) => {
+          println!(" {} ms away from local system time", time.offset());
+          time.push(time);
+        }
+        Err(_) => {
+          println!(" ? [response took too long]")
+        }
+      };
+
+      let mut offsets = Vec::with_capacity(servers.len());
+      let mut offset_weights = Vec::with_capacity(servers.len());
+
+      for time in &times {
+        let offset = time.offset() as f64;
+        let delay = time.delay() as f64;
+        
+        let weight = 1_000_000.0 / (delay * delay);
+        if weight.is_finite() {
+          offsets.push(offset);
+          offset_weights.push(weight);
+        }
+      }
+
+      let avg_offset = weighted_mean(&offsets, &offset_weights);
+
+      Ok(avg_offset)
     }
 }
