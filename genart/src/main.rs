eef28410ee3f040ad75f3821ebe7286223e8dfc3
elliexcoding <ladieexstepho@gmail.com>
2023-08-02 21:07:18 +0900

    feat: Spawn thread then countdown

--- genart/src/main.rs
+++ genart/src/main.rs
@@ -1,5 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use std::{thread, time};
 
-    let add = |a, b| { a + b };
+fn main () {
+    let start = time::Instant::now();
+
+    let handler = thread::span(|| {
+        let pause = time::Duration::from_millis(300);
+        thread::sleep(pause);
+    });
+
+    handlerjoin().unwrap();
+
+    let elapsed = time::Instant::now();
+
+    println!("Elapsed: {:?}", elapsed.duration_since(start))
 }
