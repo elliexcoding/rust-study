d0a447df8b8412485f307d2a9d6ff03137a52aa2
elliexcoding <ladieexstepho@gmail.com>
2023-08-07 21:18:45 +0900

    refactor: Use pop to handle modifications for join

--- genart/src/main.rs
+++ genart/src/main.rs
@@ -1,16 +1,25 @@
 use std::{thread, time};
 
+
 fn main () {
-    let start = time::Instant::now();
+    for n in 1..1001 {
+        let mut handlers: Vec<thread::JoinHandle<()>> = Vec::with_capacity(n);
 
-    let handler = thread::span(|| {
-        let pause = time::Duration::from_millis(300);
-        thread::sleep(pause);
-    });
+        let start = time::Instant::now();
 
-    handlerjoin().unwrap();
+        for _m in 0 .. n {
+            let handle = thread::spawn(|| {
+                let pause = time::Duration::from_millis(20);
+                thread::sleep(pause);
+            });
+            handlers.push(handle);
+        }
 
-    let elapsed = time::Instant::now();
+        while let Some(handle) = handlers.pop() {
+            handle.join();
+        }
 
-    println!("Elapsed: {:?}", elapsed.duration_since(start))
+        let finish = time::Instant::now();
+        println!("{}\t{:02}", n, finish.duration_since(start).as_secs());
+    }
 }
