20519507f01c96e8d5c1b69f5a3f8d73253b5537
elliexcoding <ladieexstepho@gmail.com>
2023-08-13 09:22:31 +0900

    refactor: Added skeleton calls for processing input and SVG gen

--- genart/src/main.rs
+++ genart/src/main.rs
@@ -1,25 +1,31 @@
-use std::{thread, time};
+use std::env;
 
+use svg::node::element::path::{Command, Data, Position};
+use svg::node::element::{Path, Rectangle};
+use svg::Document;
 
-fn main () {
-    for n in 1..1001 {
-        let mut handlers: Vec<thread::JoinHandle<()>> = Vec::with_capacity(n);
+#[derive(Debug, Clone, Copy)]
+enum Operation {
+    Forward(isize),
+    TurnLeft,
+    TurnRight,
+    Home,
+    Noop(usize),
+}
 
-        let start = time::Instant::now();
-
-        for _m in 0 .. n {
-            let handle = thread::spawn(|| {
-                let pause = time::Duration::from_millis(20);
-                thread::sleep(pause);
-            });
-            handlers.push(handle);
-        }
-
-        while let Some(handle) = handlers.pop() {
-            handle.join();
-        }
-
-        let finish = time::Instant::now();
-        println!("{}\t{:02}", n, finish.duration_since(start).as_secs());
+fn main() {
+    fn parse(input: &str) -> Vec<Operation> {
+        let mut steps = Vec::<Operation>::new();
     }
+
+
+    let args = env::args().collect::<Vec<String>>();
+    let input = args.get(1).unwrap();
+    let default_filename = format!("{}.svg", input);
+    let save_to = args.get(2).unwrap_or(&default_filename);
+
+    let operations = parse(input);
+    let path_data = convert(&operations);
+    let document = generate_svg(path_data);
+    svg::save(save_to, &document).unwrap();
 }
