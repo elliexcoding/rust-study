1b6a60203d42acd27a78704c99c72738a97f826a
elliexcoding <ladieexstepho@gmail.com>
2023-08-26 15:37:20 +0900

    feat: Bidreictional channel system

--- genart/src/main.rs
+++ genart/src/main.rs
@@ -4,16 +4,55 @@ extern crate crossbeam;
 use std::thread;
 use crossbeam::channel::unbounded;
 
-fn main() {
-  let (tx, rx) = unbounded();
+// Cross-channel usage
+use crate::ConnectvityCheck::*;
 
-  thread::spawn(move || {
-    tx.send(42).unwrap();
+// fn main() {
+//   let (tx, rx) = unbounded();
+//
+//   thread::spawn(move || {
+//     tx.send(42).unwrap();
+//   });
+//
+//   select!{
+//     recv(rx) -> msg => {
+//       println!("Received {}", msg.unwrap());
+//     }
+//   }
+// }
+
+#[derive(Debug)]
+enum ConnectvityCheck {
+  Ping,
+  Pong,
+  Pang,
+}
+
+fn main() {
+  let n_messages = 3;
+  let (requests_tx, requests_rx) = unbounded();
+  let (responses_tx, responses_rx) = unbounded();
+
+  thread::span(move || loop {
+    match requests_rx.recv().unwrap() {
+      Pong => eprintln!("Unexpected pong response"),
+      Ping => responses_tx.send(Pong).unwrap(),
+      Pang => return,
+    }
   });
 
-  select!{
-    recv(rx, msg) => {
-      println!("Received {}", msg.unwrap());
+  for _ in 0..n_messages {
+    requests_tx.send(Ping).unwrap();
+  }
+
+  requests_tx.send(Pang).unwrap();
+
+  for _ in 0..n_messages {
+    select! {
+        recv(responses_rx) -> msg => {
+            println!("Received {:?}", msg.unwrap());
+        }
     }
   }
+
 }
