54dc01789d47bb35736828f6c91466b66b8880c3
elliexcoding <ladieexstepho@gmail.com>
2023-09-13 21:19:11 +0900

    feat: Timer to show kill operation

--- chirpy-os/src/main.rs
+++ chirpy-os/src/main.rs
@@ -1,3 +1,17 @@
+use std::time;
+use std::process;
+use std::thread::{sleep};
+
 fn main() {
-    println!("Hello, world!");
+    let delay = time::Duration::from_secs(1);
+
+    let pid = process::id();
+    println!("My pid is {}", pid);
+
+    for i in 1..=60 {
+        sleep(delay);
+        println!("{} seconds have passed", i);
+    }
+
+    // kill -SIGSTOP {PID}
 }
