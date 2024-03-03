8ff656e9b33560eee36a5578243f6f91068a0cac
elliexcoding <ladieexstepho@gmail.com>
2024-03-03 13:49:30 +0900

    feat: Added test for subscription

--- zero2hero/tests/health_check.rs
+++ zero2hero/tests/health_check.rs
@@ -1,4 +1,4 @@
-use axum::{body::Body, http::Request};
+use axum::{body::Body, http::{self, Request, StatusCode}};
 use http_body_util::BodyExt; // for `collect`
 
 use tower::ServiceExt;
@@ -16,4 +16,42 @@ async fn health_check_works() {
     assert_eq!(response.status(), 200);
     let body = response.into_body().collect().await.unwrap().to_bytes();
     assert_eq!(body, "Hello World".as_bytes());
+}
+
+#[tokio::test]
+async fn subscribe_returns_a_200_for_valid_form_data() {
+    let app = app();
+    let body = "name=frosty&20wolf&email=frosty_wolf%40gmail.com";
+    let response = app
+        .oneshot(
+            Request::builder()
+                .method(http::Method::POST)
+                .uri("/subscribe")
+                .header("content-type", "application/x-www-form-urlencoded")
+                .body(Body::from(body))
+                .unwrap()
+        )
+        .await
+        .unwrap();
+
+    assert_eq!(response.status(), 200);
+}
+
+#[tokio::test]
+async fn subscribe_returns_a_400_for_invalid_form_data() {
+    let app = app();
+    let body = "name=frosty&20wolf";
+    let response = app
+        .oneshot(
+            Request::builder()
+                .method(http::Method::POST)
+                .uri("/subscribe")
+                .header("content-type", "application/x-www-form-urlencoded")
+                .body(Body::from(body))
+                .unwrap()
+        )
+        .await
+        .unwrap();
+
+    assert_eq!(response.status(), 400);
 }
