846df4e4525f2bb40c7db433207235a4878a5dea
eugene <ladieexstepho@gmail.com>
2024-06-30 01:10:50 +0900

    feat: Load in Postgres Pool connection from docker-config file

--- zero2hero/src/startup.rs
+++ zero2hero/src/startup.rs
@@ -1,16 +1,14 @@
+use sqlx::PgPool;
 use crate::{build_routes};
 use crate::configuration::get_configuration;
+use crate::cmd;
 use sqlx::postgres::{PgPoolOptions};
-use tower_http::{classify::ServerErrorsFailureClass, trace::TraceLayer};
-use tracing::{info_span, Span};
-use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 pub async fn run() {
-    let configuration = get_configuration().expect("Failed to read configuration.");
-    println!("Starting server at http://0.0.0.0:{}", configuration.application_port);
-    let addr = format!("0.0.0.0:{}", configuration.application_port);
-    println!("Connecting to Postgres...");
-    println!("Connected to Postgres.");
+    let args = cmd::parse();
+    let configuration = get_configuration(args.config_path).expect("Failed to read configuration.");
+    println!("Starting server at http://0.0.0.0:{}", &configuration.application_port);
+    let addr = format!("0.0.0.0:{}", &configuration.application_port);
     // run our app with hyper, listening globally on port 3000
     println!("Starting up the server...");
     let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
@@ -19,12 +17,14 @@ pub async fn run() {
         listener.local_addr().unwrap()
     );
 
+    println!("{}", &configuration.database.connection_string());
+
     let pool = PgPoolOptions::new()
         .max_connections(50)
-        .acquire_timeout(std::time::Duration::from_secs(3))
-        .connect(&configuration.database.connection_string())
-        .await
+        .acquire_timeout(std::time::Duration::from_secs(1))
+        .connect_lazy(&configuration.database.connection_string())
         .expect("Failed to connect to Postgres.");
+
 
     let app = build_routes(pool);
     axum::serve(listener, app.into_make_service()).await.unwrap();
