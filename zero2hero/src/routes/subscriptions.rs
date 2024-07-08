b2c8214362038bc6adfee2ebed92ee4a4a8f842e
eugene <ladieexstepho@gmail.com>
2024-07-08 21:35:36 +0900

    feat: Set tracing as request span for subscriptions

--- zero2hero/src/routes/subscriptions.rs
+++ zero2hero/src/routes/subscriptions.rs
@@ -6,7 +6,7 @@ use axum::Form;
 use axum::http::StatusCode;
 use axum::debug_handler;
 use tracing::{debug, error, info, span, warn, Level, info_span};
-use tracing:: Instrument;
+use tracing::Instrument;
 
 #[derive(Debug, serde::Deserialize)]
 pub struct FormData {
@@ -14,19 +14,22 @@ pub struct FormData {
     name: String,
 }
 
-#[tracing::instrument]
 #[debug_handler]
+#[tracing::instrument(
+    name = "Adding a new subscriber",
+    skip(_form, db),
+    fields(
+        requestid = % Uuid::new_v4(),
+        subscriber_email = %_form.email,
+        subscriber_name = %_form.name
+    )
+)]
 pub async fn subscribe(
     State(db): State<PgPool>,
     Form(_form): Form<FormData>,
 ) -> Result<(), StatusCode> {
-    let request_id = Uuid::new_v4();
-    let request_span = info_span!("Adding a new subscriber.", %request_id, subscriber_email = %_form.email, subscriber_name = %_form.name);
-    // Not good to enter in an async function
-    let _request_span_guard = request_span.enter();
-    info!("request id {} - Adding '{}' '{}' to the database", request_id, _form.email, _form.name);
     let query_span = info_span!("Saving new subscriber details in the database.");
-    let result = sqlx::query!(
+    match sqlx::query!(
         r#"
         INSERT INTO subscriptions (id, email, name, subscribed_at)
         VALUES ($1, $2, $3, $4)
@@ -38,15 +41,13 @@ pub async fn subscribe(
     )
         .execute(&db)
         .instrument(query_span)
-        .await;
-
-    match result {
+        .await
+    {
         Ok(_) => {
-            info!("request id {} - New subscriber details saved", request_id);
             Ok(())
         }
         Err(e) => {
-            error!("request_id {} - Failed to execute query: {}", request_id, e);
+            error!("Failed to execute query: {}", e);
             Err(StatusCode::BAD_REQUEST)
         }
     }
