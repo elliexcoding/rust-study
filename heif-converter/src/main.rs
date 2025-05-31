489652dfb2e7e70d01ccdb3fc833b9f3836cf4c2
eugene <ladieexstepho@gmail.com>
2025-05-31 13:53:24 +0900

    feat: Open HEIC file

--- heif-converter/src/main.rs
+++ heif-converter/src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
+use libheif_rs::{Channel, ColorSpace, HeifContext, ItemId, LibHeif, Result, RgbChroma};
+use std::ffi;
+use std::ptr;
+
+fn main() -> Result<()>{
     println!("Hello, world!");
+    let lib = LibHeif::new();
+    let ctx = HeifContext::read_from_file(
+        "./data/IMG_1415.HEIC"
+    )?;
+    let handle = ctx.primary_image_handle()?;
+    assert_eq!(handle.width(), 5712);
+    assert_eq!(handle.height(), 4284);
+    
+    Ok(())
 }
